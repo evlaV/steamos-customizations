evlaV/steamos-customizations

commit f5c73b7312374cdcc59c0c8f3b8ec59dff109ffa

swap: hibernation: reset boot counter on resume from hibernation
Chainloader increments and checks the boot attempts counter. The
steamos-boot service reset it to 0. Resume form hibernation loads state
from disk and targets are skipped. Hence counter doesn't get reset. Add
reset mechanism to hibernation script which will reset the counter at
resume from hibernation.

Relates: https://gitlab.steamos.cloud/holo-team/tasks/-/issues/1569
Signed-off-by: Muhammad Usama Anjum <usama.anjum@collabora.com>

swap/hibernation/hibernate-swap-helper.sh
@@ -139,6 +139,10 @@ create_swap() {
 }
 
 cleanup_swap() {
+	# Set boot counter to 0 after successful resume
+	steamos-bootconf --set boot-attempts 0
+	echo "The boot-attempt counter has been reset."
+
 	if [ ! -f "$METADATA_FILE" ]; then
 		echo "No hibernation swap metadata found"
 		exit 0

commit 0e8cc9dca8a3eaf35a7afe06db48820d2b778048

misc: Move boot counter reset to a separate service
Add separate service for boot counter reset on successful resume from
hibernation. A separate service would be best because it would be
enabled by default and its completely separate from
hibernate-clear.service, which is optional.

We could have hooked to systemd-hibernate-clear.service. But in normal
successful resume from hibernation scenerio, it doesn't runs as it has a
precondition on EFI hibernation variable. This variable gets cleaned by
the kernel earlier.

Relates: https://gitlab.steamos.cloud/holo-team/tasks/-/issues/1569
Fixes: f5c73b731237 ("swap: hibernation: reset boot counter on resume from hibernation")
Signed-off-by: Muhammad Usama Anjum <usama.anjum@collabora.com>

misc/systemd/steamos-boot-counter-reset.service.in
@@ -1,0 +1,21 @@
+#  SPDX-License-Identifier: LGPL-2.1+
+#
+#  This file is part of steamos-customizations.
+#
+#  steamos-customizations is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Lesser General Public License as
+#  published by the Free Software Foundation; either version 2.1 of the License,
+#  or (at your option) any later version.
+
+[Unit]
+Description=Reset Boot Counter
+After=systemd-hibernate-clear.service
+Wants=systemd-hibernate-clear.service
+
+[Service]
+Type=oneshot
+ExecStart=/usr/bin/steamos-bootconf --set boot-attempts 0
+RemainAfterExit=true
+
+[Install]
+WantedBy=systemd-hibernate-clear.service

swap/hibernation/hibernate-swap-helper.sh
@@ -139,10 +139,6 @@ create_swap() {
 }
 
 cleanup_swap() {
-	# Set boot counter to 0 after successful resume
-	steamos-bootconf --set boot-attempts 0
-	echo "The boot-attempt counter has been reset."
-
 	if [ ! -f "$METADATA_FILE" ]; then
 		echo "No hibernation swap metadata found"
 		exit 0