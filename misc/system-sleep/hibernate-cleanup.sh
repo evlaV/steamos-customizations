b37fb0071e1bf2993cf0e90ede5e02de90524bed
Muhammad Usama Anjum <usama.anjum@collabora.com>
2025-11-11 18:18:16 +0500

    Hibernation: Add system sleep hook
    The hibernate-cleanup service triggered when systemd-hibernate-clear
    service ran. This systemd service ran at strange points such as when
    Network Manager reloaded on a network cable (re)plug.
    
    The clean solution is instead to add a hook in system-sleep which only
    gets called while resuming from hibernation.
    
    Relates: https://gitlab.steamos.cloud/holo-team/tasks/-/issues/1869
    Suggested-by: Ludovico de Nittis <ludovico.denittis@collabora.com>
    Signed-off-by: Muhammad Usama Anjum <usama.anjum@collabora.com>

---
+++ misc/system-sleep/hibernate-cleanup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Copyright © 2025 Valve Corporation.
+
+# This script is designed to be invoked by systemd-sleep.
+# It expects 2 args: <pre|post> and the requested sleep mode
+
+set -eu
+
+declare op=${2:-${SYSTEMD_SLEEP_ACTION:-unspecified-sleep-mode}}
+
+case ${1:-} in
+  post)
+    case "$op" in
+      hibernate)
+        logger "system-sleep: Running hibernate cleanup after $op"
+        /usr/lib/steamos/hibernate-swap-helper.sh cleanup 2>&1 | logger
+        ;;
+    esac
+    ;;
+esac
