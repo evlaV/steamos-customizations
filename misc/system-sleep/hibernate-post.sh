evlaV/steamos-customizations

commit 190c8c4f148802448f8fd0330e99337d13288742

misc: Move boot counter reset to a separate service
Add separate service for boot counter reset on successful resume from
hibernation. A separate service would be best because it would be
enabled by default and its completely separate from
hibernate-clear.service, which is optional.

We could have hooked to systemd-hibernate-clear.service. But in normal
successful resume from hibernation scenerio, it doesn't runs as it has a
precondition on EFI hibernation variable. This variable gets cleaned by
the kernel earlier.

Relates: https://gitlab.steamos.cloud/holo-team/tasks/-/issues/1569
Fixes: f5c73b731237 ("swap: hibernation: reset boot counter on resume from hibernation")
Signed-off-by: Muhammad Usama Anjum <usama.anjum@collabora.com>

misc/systemd/steamos-boot-counter-reset.service.in
@@ -1,0 +1,20 @@
+#  SPDX-License-Identifier: LGPL-2.1+
+#
+#  This file is part of steamos-customizations.
+#
+#  steamos-customizations is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Lesser General Public License as
+#  published by the Free Software Foundation; either version 2.1 of the License,
+#  or (at your option) any later version.
+
+[Unit]
+Description=Reset boot counter after resuming from hibernation
+After=systemd-hibernate-clear.service
+Wants=systemd-hibernate-clear.service
+
+[Service]
+Type=oneshot
+ExecStart=/usr/bin/steamos-bootconf --set boot-attempts 0
+
+[Install]
+WantedBy=systemd-hibernate-clear.service

swap/hibernation/hibernate-swap-helper.sh
@@ -139,10 +139,6 @@ create_swap() {
 }
 
 cleanup_swap() {
-	# Set boot counter to 0 after successful resume
-	steamos-bootconf --set boot-attempts 0
-	echo "The boot-attempt counter has been reset."
-
 	if [ ! -f "$METADATA_FILE" ]; then
 		echo "No hibernation swap metadata found"
 		exit 0

commit 1f36133ea227c31702edcbee0c7af08f4be8f5a4

misc: Move boot counter reset to systemd post-sleep hook
Move boot counter reset functionality from a systemd service to the
system-sleep infrastructure. The existing implementation is triggered
multiple times after resume as `systemd-hibernate-clear.service` gets
triggered multiple times, is skipped due to it's required conditions not
being met, but this isn't classed as a "fail" in systemd terms, thus
`steamos-boot-counter-reset.service` is run.

Their doesn't seem to be a good way to describe the required
relationship as a service unit and the system-sleep infrastructure
provides a cleaner approach. We also have an existing script running at
this point, we can integrate it into that.

Rename the script to make it more generically named.

Relates: https://gitlab.steamos.cloud/holo-team/tasks/-/issues/2003
Fixes: 190c8c4 ("misc: Move boot counter reset to a separate service")
Signed-off-by: Martyn Welch <martyn.welch@collabora.com>

misc/system-sleep/hibernate-post.sh
@@ -14,6 +14,8 @@ case ${1:-} in
       hibernate)
         logger "system-sleep: Running hibernate cleanup after $op"
         /usr/lib/steamos/hibernate-swap-helper.sh cleanup 2>&1 | logger
+        logger "system-sleep: Reset boot counter after $op"
+        /usr/bin/steamos-bootconf --set boot-attempts 0 2>&1 | logger
         ;;
     esac
     ;;

misc/systemd/steamos-boot-counter-reset.service.in
@@ -1,20 +1,0 @@
-#  SPDX-License-Identifier: LGPL-2.1+
-#
-#  This file is part of steamos-customizations.
-#
-#  steamos-customizations is free software; you can redistribute it and/or
-#  modify it under the terms of the GNU Lesser General Public License as
-#  published by the Free Software Foundation; either version 2.1 of the License,
-#  or (at your option) any later version.
-
-[Unit]
-Description=Reset boot counter after resuming from hibernation
-After=systemd-hibernate-clear.service
-Wants=systemd-hibernate-clear.service
-
-[Service]
-Type=oneshot
-ExecStart=/usr/bin/steamos-bootconf --set boot-attempts 0
-
-[Install]
-WantedBy=systemd-hibernate-clear.service